12aa3d4ba9ecd5db34a0007a1ea9f3e5ed02c516
taylor <tfar88@hotmail.com>
2020-05-13 15:46:37 -0400

    axios was added for api

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,17 @@
-console.log('i am test.js')
+import axios from 'axios';
+async function getResults(query){
+try{
+    const res = await axios(`https://forkify-api.herokuapp.com/api/search?q=${query}`)
+    const recipes = res.data.recipes
+    console.log(recipes)
+}
+catch(error)
+{
+    alert(error)
+}
+  
 
-export default 478521212
+
+}
+
+getResults('carrot');
