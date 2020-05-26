4c6393f13ad1b7d76f698ab6c7d168956feb68cb
Taylor Farley <twfarley88@gmail.com>
2020-05-26 09:34:20 -0400

    build search view start

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,43 @@
-import axios from 'axios';
-async function getResults(query){
-try{
-    const res = await axios(`https://forkify-api.herokuapp.com/api/search?q=${query}`)
-    const recipes = res.data.recipes
-    console.log(recipes)
-}
-catch(error)
-{
-    alert(error)
-}
-  
+import Search from './models/Search'
+
+//global state of the app
+// search object
+// current recipe object
+// shopping list object
+// liked recipes
+
+const state = {}
 
 
+const controlSearch = async() =>{
+    // 1. get query from view
+    const query = 'pizza' //TODO
+   
+    if (query){
+        //2)new search object and add to state
+        state.search = new Search(query);
+
+        //3) 
+        //prepare ui for results
+
+        //4) do the serach for recipes
+        
+       await state.search.getResults()
+        //5)
+        //render the results on UI
+        console.log(state.search.result)
+    }
+    //
+
+    console.log('SUB PLAYA')
 }
 
-getResults('carrot');
+document.querySelector('.search').addEventListener('click', e=> {
+    e.preventDefault();
+    controlSearch();
+})
+
+
+
+
+
